5813bb49bf970d6b7f5092c2b184b19836184105
ydd <1498396121@qq.com>
2026-02-01 16:25:53 +0800

    第seven次提交

--- day07/src/homework/UnSecurityCouncil.java
+++ day07/src/homework/UnSecurityCouncil.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UnSecurityCouncil {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请录入一个国家");
+        System.out.println("请录入一个国家名字");
         String country = scanner.next();
         boolean b = isMember(country);
         System.out.println(b?"是常任理事国":"不是常任理事国");
